171a4a3373e26b4bae51b4c6bc5891fb1079ee6c
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-11-14 15:26:00 +0800

    adding logout functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import Cart from "./Cart";
+import { useDispatch } from "react-redux";
+import { authActions } from "../../store/auth-slice";
 
 export default function Header() {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(authActions.Logout());
+  };
   return (
     <header>
       <nav className="mb-10 p-6 shadow-lg">
@@ -9,7 +16,15 @@ export default function Header() {
           <li className="text-xl font-medium sm:text-2xl">
             Redux Shopping App
           </li>
-          <Cart />
+          <li className="flex items-center gap-5">
+            <Cart />
+            <button
+              onClick={logoutHandler}
+              className="rounded-lg bg-orange-400 px-2 py-1 text-lg font-medium text-white hover:bg-orange-300 sm:px-4 sm:py-2 sm:text-xl"
+            >
+              Logout
+            </button>
+          </li>
         </ul>
       </nav>
     </header>
